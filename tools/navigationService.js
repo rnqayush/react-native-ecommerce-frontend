fddce36293cc93b7b252f7478ae6ccfb55d3686a
Ayush Mishra <rnq.ayush@gmail.com>
2021-02-17 20:55:47 +0530

    first commit

---
+++ tools/navigationService.js
@@ -0,0 +1,20 @@
+
+/*let navigator;
+
+const setTopLevelNavigator = navigatorRef => {
+    navigator = navigatorRef;
+}
+
+const navigate = (routeName, params) => {
+    navigator.dispatch(
+        NavigationActions.navigate({
+            routeName,
+            params
+        })
+    );
+};
+
+export default {
+    navigate,
+    setTopLevelNavigator
+}; */
